1b624a9e3bd6c3e32fd4244c0ccd946dc17d8454
yxurrbzallkd <60686597+yxurrbzallkd@users.noreply.github.com>
2021-01-03 17:33:12 +0200

    Add files via upload

---
+++ inc/build/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../program"
+  "../program.pdb"
+  "CMakeFiles/program.dir/fungera.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
