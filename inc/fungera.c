c5c49b0544b399cc22b1fe186b5fb93e4260754a
yxurrbzallkd <60686597+yxurrbzallkd@users.noreply.github.com>
2021-01-17 15:54:26 +0200

    improved comments

--- inc/fungera.c
+++ inc/fungera.c
@@ -3,47 +3,51 @@
 #include <stdio.h>
 #include <time.h>
 
-int MAX_N_OF_ERRORS = 5000;
-int MAX_ITERS_WITHOUT_REPRODUCTION = 100000;
-int TEMPLATE[1024];
-int STACK_SIZE = 8;
 
-int COMMAND_LENGTH = 5;
 
 typedef struct {
 	int size;
 	char *content;
 } array;
 
-char commands[25] = {'.', ':', 'a', 'b', 'c', 'd', 'x', 'y', '^', 'v', '>', '<', '&', '?', '1', '0', '-', '+', '~', 'L', 'W', '@', '$', 'S', 'P'};
-int N_OF_COMMANDS = 25;
 
+int STACK_SIZE = 8;
 int id = 0;
 typedef struct {
 	int parent_id;
 	int id;
+	//location in memory
 	int startx;
 	int starty;
+	//size of body's memory chunk
 	int width;
 	int height;
+	//instruction pointer (!not absolute: relative position in the body)
 	int ptrx;
 	int ptry;
+	//direction in which we currently readcode
 	int deltax;
 	int deltay;
+	//Registers
+	//register a
 	int ax;
 	int ay;
+	//register b
 	int bx;
 	int by;
+	//register c
 	int cx;
 	int cy;
+	//register d
 	int dx;
 	int dy;
+	//stack
 	int stackx[8];
 	int stacky[8];
 	int stacktop;
 	int errors;
-	// reproduction
-	int reproduction_cycle;
+	// Reproduction
+	int reproduction_cycle; //gets reset to 0 every time organism reproduces
 	// child
 	int childx;
 	int childy;
@@ -60,8 +64,6 @@ typedef struct {
 } table_of_organisms;
 
 
-int kill_organism(organism org, array* memory);
-
 void table_create(table_of_organisms* table, int size) {
 	table->top = 0;
 	table->size = size;
@@ -69,14 +71,19 @@ void table_create(table_of_organisms* table, int size) {
 	table->organisms = calloc(size, sizeof(organism*));
 }
 
-
 void print_queue(table_of_organisms* table) {
 	for (int j = 0; j < table->top; j++) {
-//		printf("%d id %d at (%d %d) errors %d\n", j, table->organisms[j].id, table->organisms[j].startx, table->organisms[j].starty, table->organisms[j].errors);
+		printf("%d id %d at (%d %d) errors %d\n", j, table->organisms[j].id, table->organisms[j].startx, table->organisms[j].starty, table->organisms[j].errors);
 	}
 }
 
+int kill_organism(organism org, array* memory);
+
+int MAX_N_OF_ERRORS = 5000;
+int MAX_ITERS_WITHOUT_REPRODUCTION = 100000;
 void update_queue(table_of_organisms* table, array* memory) {
+	// Sort organisms by number of errors (in ascending order)
+	// insertion sort
 	int temp;
 	for (int j = 1; j < table->top; j++) {
 		for (int i = j; i > 0; i--) {
@@ -88,24 +95,25 @@ void update_queue(table_of_organisms* table, array* memory) {
 			}
 		}
 	}
-	//print_queue(q);
 	int end = table->top;
 	for (int i = table->top-1; i >= 0; i--) {
-		//printf("iteration %d organism %d (errors %d)\n", i, (q->organisms[i]).id, (q->organisms[i]).errors);
 		if ((table->organisms[i].errors) > MAX_N_OF_ERRORS) {
 			end = i;
 			break;
 		}
 	}
+	// Kill organisms that made too many errors
 	for (int i = end; i < table->top; i++) {
 		if (!(kill_organism(table->organisms[table->organisms_order[i]], memory) == 0)) {
 			return;
 		}
 	}
+	// update table size
 	table->top = end;
 }
 
 int append_queue(table_of_organisms* table, organism* org) {
+	// add an organism in the topmost free position of table
 	if (table->top == table->size) {
 		return -1;
 	}
@@ -116,44 +124,54 @@ int append_queue(table_of_organisms* table, organism* org) {
 	return 0;
 }
 
+// COMMANDS
+char REGISTER_A = 'a';
+char REGISTER_B = 'b';
+char REGISTER_C = 'c';
+char REGISTER_D = 'd';
+char commands[25] = {'.', ':', 'a', 'b', 'c', 'd', 'x', 'y', '^', 'v', '>', '<', '&', '?', '1', '0', '-', '+', '~', 'L', 'W', '@', '$', 'S', 'P'};
+int N_OF_COMMANDS = 25;
+
+
 int command_symbol_to_code(char s) {
 	for (int i = 0; i < N_OF_COMMANDS; i++) {
 		if (s == commands[i]) {
 			return i;
 		}
 	}
-	//printf("unknown command %c\n", s);
 	return -1;
 }
 
+int COMMAND_LENGTH = 5;
 /*
-'.' : 00000001 : 'no_operation',
-':' : 00000010 : 'no_operation',
-'a' : 00000011 : 'no_operation',
-'b' : 00000100 : 'no_operation',
-'c' : 00000101 : 'no_operation',
-'d' : 00000110 : 'no_operation',
-'x' : 00000111 : 'no_operation',
-'y' : 00001000 : 'no_operation',
-'^' : 00001001 : 'move_organism_pointer_up',
-'v' : 00001010 : 'move_organism_pointer_down',
-'>' : 00001011 : 'move_organism_pointer_right',
-'<' : 00001100 : 'move_organism_pointer_left',
-'&' : 00001101 : 'find_template',
-'?' : 00001110 : 'if_not_zero',
-'1' : 00001111 : 'one',
-'0' : 00010000 : 'zero',
-'-' : 00010001 : 'decrement',
-'+' : 00010010 : 'increment',
-'~' : 00010011 : 'subtract',
-'L' : 00010100 : 'load_inst',
-'W' : 00010101 : 'write_inst',
-'@' : 00010110 : 'allocate_child',
-'$' : 00010111 : 'split_child',
-'S' : 00011000 : 'push',
-'P' : 00011001 : 'pop',
+'.' : 000001 : 'no_operation',
+':' : 000010 : 'no_operation',
+'a' : 000011 : 'no_operation',
+'b' : 000100 : 'no_operation',
+'c' : 000101 : 'no_operation',
+'d' : 000110 : 'no_operation',
+'x' : 000111 : 'no_operation',
+'y' : 001000 : 'no_operation',
+'^' : 001001 : 'move_organism_pointer_up',
+'v' : 001010 : 'move_organism_pointer_down',
+'>' : 001011 : 'move_organism_pointer_right',
+'<' : 001100 : 'move_organism_pointer_left',
+'&' : 001101 : 'find_template',
+'?' : 001110 : 'if_not_zero',
+'1' : 001111 : 'one',
+'0' : 010000 : 'zero',
+'-' : 010001 : 'decrement',
+'+' : 010010 : 'increment',
+'~' : 010011 : 'subtract',
+'L' : 010100 : 'load_inst',
+'W' : 010101 : 'write_inst',
+'@' : 010110 : 'allocate_child',
+'$' : 010111 : 'split_child',
+'S' : 011000 : 'push',
+'P' : 011001 : 'pop',
 */
 
+// MEMORY
 /*
 free :      00000000
 allocated : 00000001
@@ -166,24 +184,24 @@ int set_register_value(organism* org, int register_code, int x, int y) {
 		return -1;
 	}
 	char reg = commands[register_code];
-	if (reg == 'a') {
+	if (reg == REGISTER_A) {
 		org->ax = x;
 		org->ay = y;
 	}
-	else if (reg == 'b') {
+	else if (reg == REGISTER_B) {
 		org->bx = x;
 		org->by = y;
 	}
-	else if (reg == 'c') {
+	else if (reg == REGISTER_C) {
 		org->cx = x;
 		org->cy = y;
 	}
-	else if (reg == 'd') {
+	else if (reg == REGISTER_D) {
 		org->dx = x;
 		org->dy = y;
 	}
 	else {
-		//printf("register %c doesn't exist\n", reg);
+		// register doesn't exist
 		return -1;
 	}
 	return 0;
@@ -191,28 +209,28 @@ int set_register_value(organism* org, int register_code, int x, int y) {
 
 int get_register_value(organism* org, int register_code, int* x, int* y) {
 	if ((register_code < 0) || (register_code >= N_OF_COMMANDS)) {
-		//printf("unknown command code %d\n", register_code);
+		// unknown command code
 		return -1;
 	}
 	char reg = commands[register_code];
-	if (reg == 'a') {
+	if (reg == REGISTER_A) {
 		*x = org->ax;
 		*y = org->ay;
 	}
-	else if (reg == 'b') {
+	else if (reg == REGISTER_B) {
 		*x = org->bx;
 		*y = org->by;
 	}
-	else if (reg == 'c') {
+	else if (reg == REGISTER_C) {
 		*x = org->cx;
 		*y = org->cy;
 	}
-	else if (reg == 'd') {
+	else if (reg == REGISTER_D) {
 		*x = org->dx;
 		*y = org->dy;
 	}
 	else {
-		//printf("register %d (%c) doesn't exist\n", register_code, reg);
+		// register doesn't exist
 		return -1;
 	}
 	return 0;
@@ -257,12 +275,14 @@ int organism_create(organism* org, int startx, int starty, int width, int height
 	return 0;
 }
 
+
 void move_organism_pointer(organism* org) {
 	org->ptrx += org->deltax;
 	org->ptry += org->deltay;
 }
 
 void print_array(array* arr) {
+	// print command symbols in memory
 	for (int i = 0; i < arr->size; i++) {
 		for (int j = 0; j < arr->size; j++) {
 			printf("%c", commands[arr->content[i*arr->size + j] >> 2]);
@@ -273,6 +293,7 @@ void print_array(array* arr) {
 }
 
 void print_array_mask(array* arr) {
+	// print types of memory cells (free, allocated, active)
 	for (int i = 0; i < arr->size; i++) {
 		for (int j = 0; j < arr->size; j++) {
 			printf("%d", 3 & arr->content[i*arr->size + j]);
@@ -376,6 +397,7 @@ int memory_free(array* memory, int x, int y, int w, int h) {
 }
 
 char array_get(array* arr, int x, int y) {
+	// get byte (char) in position (x, y)
 	if ((x < 0) || (x >= arr->size) || (y < 0) || (y >= arr->size)) {
 		return -1;
 	}
@@ -400,6 +422,7 @@ int array_get_cell_type(array* arr, int x, int y) {
 }
 
 int array_set(array* arr, int x, int y, char c) {
+	// set value of byte in position (x, y)
 	if ((x < 0) || (x >= arr->size) || (y < 0) || (y >= arr->size)) {
 		return -1;
 	}
@@ -475,6 +498,7 @@ int down(organism* org) {
 }
 
 int Stack(array* memory, organism* org) {
+	// push value of register into stack
 	char reg = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	int x;
 	int y;
@@ -491,6 +515,7 @@ int Stack(array* memory, organism* org) {
 }
 
 int Pop(array* memory, organism* org) {
+	// pop value from stack into register
 	if (org->stacktop == 0) {
 		return -1;
 	}
@@ -505,6 +530,24 @@ int Pop(array* memory, organism* org) {
 }
 
 int jumpc(array* memory, organism* org) {
+	/*go to closer option if zero, to further one if not zero
+
+	|<-ptr	  a: (0, something)
+	|		  on the next step: >
+	?xa>^
+
+	|<-ptr	  a: (0, 0)
+	|		  on the next step: >
+	?a>^
+	
+	|<-ptr   a: NOT (0, 0)
+	|		 on the next step: ^
+	?a>^
+
+	|<-ptr	 a: (something, anything)
+	|		 on the next step: ^
+	?xa>^
+	*/
 	int reg = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	if ((reg < 0) || (reg >= N_OF_COMMANDS)) {
 		return -1;
@@ -535,6 +578,7 @@ int jumpc(array* memory, organism* org) {
 }
 
 int increment(array* memory, organism* org) {
+	//increment a component \ both components of a register
 	int reg = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	if ((reg < 0) || (reg >= N_OF_COMMANDS)) {
 		return -1;
@@ -544,6 +588,7 @@ int increment(array* memory, organism* org) {
 	int xs;
 	int ys;
 	if ((commands[reg] == 'x') || (commands[reg] == 'y')) {
+		//partial increment: increment value of one of register's components
 		reg = commands[reg];
 		x = (reg == 'x') ? 1 : 0;
 		y = (reg == 'y') ? 1 : 0;
@@ -559,6 +604,7 @@ int increment(array* memory, organism* org) {
 }
 
 int decrement(array* memory, organism* org) {
+	//decrement a component \ both components of a register
 	int reg = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	if ((reg < 0) || (reg >= N_OF_COMMANDS)) {
 		return -1;
@@ -568,6 +614,7 @@ int decrement(array* memory, organism* org) {
 	int xs;
 	int ys;
 	if ((commands[reg] == 'x') || (commands[reg] == 'y')) {
+		//partial decrement: increment value of one of register's components
 		reg = commands[reg];
 		x = (reg == 'x') ? 1 : 0;
 		y = (reg == 'y') ? 1 : 0;
@@ -583,16 +630,17 @@ int decrement(array* memory, organism* org) {
 }
 
 int write_instance(array* memory, organism* org) {
-	int reg1 = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
-	int reg2 = array_get_command_code(memory, org->ptrx + org->startx+org->deltax*2, org->ptry + org->starty+org->deltay*2);
+	// write instance from one register to another
+	int regto = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
+	int regfrom = array_get_command_code(memory, org->ptrx + org->startx+org->deltax*2, org->ptry + org->starty+org->deltay*2);
 	int addressx;
 	int addressy;
 	int x;
 	int y;
-	if (!(get_register_value(org, reg2, &x, &y) == 0)) {
+	if (!(get_register_value(org, regfrom, &x, &y) == 0)) {
 		return -1;
 	}
-	if (!(get_register_value(org, reg1, &addressx, &addressy) == 0)) {
+	if (!(get_register_value(org, regto, &addressx, &addressy) == 0)) {
 		return -1;
 	}
 	if ((addressx < org->childx) || (addressx >= org->childx+org->child_width) || 
@@ -610,6 +658,7 @@ int write_instance(array* memory, organism* org) {
 }
 
 int load_instance(array* memory, organism* org) {
+	// load a vector corresponding to instance in one register into another
 	int reg_dest = array_get_command_code(memory, org->ptrx + org->startx+org->deltax*2, org->ptry + org->starty+org->deltay*2);
 	int reg_source = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	if ((reg_source < 0) || (reg_source >= N_OF_COMMANDS)) {
@@ -633,7 +682,18 @@ int load_instance(array* memory, organism* org) {
 	return 0;
 }
 
+int TEMPLATE[1024]; //limited size
+int MAX_TEMPLATE_SIZE = 1024;
 int find_template(array* memory, organism* org) {
+	// address by pattern
+	/*
+	direction: >
+	|-command symbol
+	||-register to store address
+	|| ____ template = ~ (.:..:..) = :.::.::                  (x, y) to store
+	||/    \                                                  |
+	&a.:..:.._some_symbols_that_aren't_._or_:_until_hopefully_:.::.::
+	*/
 	int x = org->ptrx + org->startx + org->deltax;
 	int y = org->ptry + org->starty + org->deltay;
 	int reg = array_get_command_code(memory, x, y);
@@ -644,7 +704,7 @@ int find_template(array* memory, organism* org) {
 	y += org->deltay;
 	int c = array_get_command_code(memory, x, y);
 	int i = 0;
-	while ((i < maxlen) && ((commands[c] == '.') || (commands[c] ==':' ))) {
+	while ((i < maxlen) && ((commands[c] == '.') || (commands[c] ==':' )) && (i < MAX_TEMPLATE_SIZE)) {
 		TEMPLATE[i] = (commands[c] == '.') ? command_symbol_to_code(':') : command_symbol_to_code('.');
 		x += org->deltax;
 		y += org->deltay;
@@ -679,6 +739,7 @@ int find_template(array* memory, organism* org) {
 }
 
 int allocate_child(array* memory, organism* org) {
+	// find a chunk of free memory and reserve it for copying yourself
 	int child_size = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	int child_start = array_get_command_code(memory, org->ptrx + org->startx+org->deltax*2, org->ptry + org->starty+org->deltay*2);
 	if ((child_size < 0) || (child_size >= N_OF_COMMANDS) || (child_start < 0) || (child_start >= N_OF_COMMANDS)) {
@@ -721,6 +782,7 @@ int allocate_child(array* memory, organism* org) {
 }
 
 int zero(array* memory, organism* org) {
+	//set value of both components of given register to 0
 	int reg = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	if ((reg < 0) || (reg >= N_OF_COMMANDS)) {
 		return -1;
@@ -732,6 +794,7 @@ int zero(array* memory, organism* org) {
 }
 
 int one(array* memory, organism* org) {
+	//set value of both components of given register to 1
 	int reg = array_get_command_code(memory, org->ptrx + org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	if ((reg < 0) || (reg >= N_OF_COMMANDS)) {
 		return -1;
@@ -743,6 +806,7 @@ int one(array* memory, organism* org) {
 }
 
 int subtract(array* memory, organism* org) {
+	//register_3 = register1 - register2
 	int reg1 = array_get_command_code(memory, org->ptrx + 
 	org->startx+org->deltax, org->ptry + org->starty+org->deltay);
 	int reg2 = array_get_command_code(memory, org->ptrx + 
@@ -768,6 +832,7 @@ int subtract(array* memory, organism* org) {
 }
 
 int split_child(array* memory, organism* org, table_of_organisms* table) {
+	//split child - create a new, independent organism
 	if ((org->child_width < 0) || (org->child_height < 0)) {
 		return -1;
 	}
@@ -790,6 +855,7 @@ int split_child(array* memory, organism* org, table_of_organisms* table) {
 }
 
 int operation(array* memory, organism* org, table_of_organisms* table, int command) {
+	//perform a given command
 	switch(command)
 	{
 		case '.': return 0;
@@ -824,7 +890,8 @@ int operation(array* memory, organism* org, table_of_organisms* table, int comma
 
 
 int life(array* memory, organism* org, table_of_organisms* table) {
-	org->reproduction_cycle++;
+	//lifecycle of an organism
+	org->reproduction_cycle++; //organism dies if it doesn't reproduce
 	move_organism_pointer(org);
 	if ((org->ptrx < 0) || (org->ptrx >= memory->size)) {
 		return -1;
@@ -839,7 +906,7 @@ int life(array* memory, organism* org, table_of_organisms* table) {
 		org->errors += 1;
 	}
 	if (org->reproduction_cycle > MAX_ITERS_WITHOUT_REPRODUCTION) {
-		org->errors += MAX_N_OF_ERRORS;
+		org->errors += MAX_N_OF_ERRORS; //organism dies if it doesn't reproduce
 	}
 	printf("O %d %d %d %c %d|", org->id, org->ptrx, org->ptry, command, code==0);
 	return 0;
@@ -868,9 +935,10 @@ int write_chunck_from_file(array* memory, int startx, int starty, int width, int
 
 
 int radiation(array* memory) {
-	int x = rand()%memory->size;
-	int y = rand()%memory->size;
-	int z = rand()%N_OF_COMMANDS;
+	// a random change to allocated memory
+	int x = rand() % memory->size;
+	int y = rand() % memory->size;
+	int z = rand() % N_OF_COMMANDS;
 	if ((3 & memory->content[x*memory->size+y]) == 2) {
 		return 0;
 	}
@@ -888,9 +956,13 @@ int cycle(table_of_organisms* table, array* memory, int i) {
 		radiation(memory);
 	}
 	update_queue(table, memory);
-	printf("\n");
+	printf("\n"); //we will read log line by line
 }
 
+/*run the program:
+gcc fungera.c -o program
+./program > log.txt
+*/
 int main() {
 	int MAX_N_OF_ORGANISMS = 10000;
 	table_of_organisms TABLE;
@@ -898,7 +970,7 @@ int main() {
 
 	srand(time(0));
 	array arr;
-	array_create(&arr, 300);
+	array_create(&arr, 128);
 
 	// Initialize the ancestor
     write_chunck_from_file(&arr, 1, 1, 23, 17, "initial.gen");
@@ -906,12 +978,12 @@ int main() {
     organism cur_o;
     organism_create(&cur_o, 1, 1, 17, 23, 0, &TABLE);
 
-	int n;
-
-
 	for (int i = 0; ((i < 1000000) && (TABLE.top != 0)); i++) {
-		n = TABLE.top;
 		cycle(&TABLE, &arr, i);
+		//if (i % 1000 == 0) {
+		//	print_array(&arr);
+		//print_array_mask(&arr);
+		//}
     }
 
 
